af24a91ced6a7260777bc29230151c6083920220
Grigory56 <95852762+Grigory56@users.noreply.github.com>
2024-10-06 21:06:53 +0700

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as divf
+from true_math import divide as divt
+
+result1 = divf(63, 7)
+result2 = divf(63, 0)
+result3 = divt(45, 9)
+result4 = divt(99,0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
